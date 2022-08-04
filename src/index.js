00efdbb51b9d2d8c7c89c173bd0d07514b71e401
Gustavo Sanmartin <gsanmartin@EN2010562.local>
2022-08-03 19:15:47 -0500

    Add logger to defaultMiddleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { restaurantActions } from './reducers/restaurantSlice';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log('Initial state', store.getState())
+const fetching = store.subscribe(()=>{})
+store.dispatch(restaurantActions.get_restaurants())
+
 root.render(
   <React.StrictMode>
     <App />
--- src/store.js
+++ src/store.js
@@ -1,14 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import logger from 'redux-logger'
 import restaurantReducer from './reducers/restaurantSlice'
 import menuReducer from './reducers/menuSlice'
 import dishReducer from './reducers/dishSlice'
+
 
 const store = configureStore({
  reducer: {
   restaurants: restaurantReducer,
   menues: menuReducer,
   dishes: dishReducer
- }
+ },
+ middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
 
 export default store
